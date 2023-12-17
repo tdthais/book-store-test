69ebd9202b958ad9b45823fd411fc367074918be
tdthais <thais.tdourado@gmail.com>
2023-12-17 12:04:07 -0300

    test/testes typeProduct

---
+++ README.md
@@ -0,0 +1,6 @@
+# Book Store API
+
+## Sobre
+
+Aplicação de testes automatizados end-to-end utilizando Cucumber e RestAssured para testar a API que pode ser encontrada em:
+https://github.com/tdthais/book-store/tree/main
---
+++ src/test/java/com/ada/bookStore/test/typeproduct/TypeProductRequest.java
@@ -0,0 +1,14 @@
+package com.ada.bookStore.test.typeproduct;
+
+
+public class TypeProductRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
